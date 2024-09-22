1dd2e1f5da8fb954c9ae4a1496822149a5f45144
Dhiren Mahajan <mahajadn@mail.uc.edu>
2024-09-22 19:22:52 -0400

    Final App Changes

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,15 @@
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import adapter from '@sveltejs/adapter-vercel';
 
 export default {
-  // Consult https://svelte.dev/docs#compile-time-svelte-preprocess
-  // for more information about preprocessors
+  // SvelteKit config
+  kit: {
+    // Configure Vercel adapter
+    adapter: adapter({
+      // Options for Vercel adapter, if any
+    }),
+  },
+
+  // Preprocess config
   preprocess: vitePreprocess(),
-}
+};
